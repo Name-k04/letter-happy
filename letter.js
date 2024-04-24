bd61c8ed49cb2fe0c2d6909fbb20a60f434578fd
Name-k04 <96805183+Name-k04@users.noreply.github.com>
2024-04-24 09:29:29 +0530

    Create letter.js

---
+++ letter.js
@@ -0,0 +1,50 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './kushi.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        My Dear Annaya 🖤,<br />
+        Im So blessed to have u in my life .Throughout our journey i didnt realised it has been a more than a year & our bond becomes strong ....<br />i loveeee youuuu :)<br />
+        I didn't realizes that im so attached to you and you means a lot to me ...<br />
+        Moreover having you my side is my best decision i have chossen for me  .I hope you to Be slefish sometimes ,that helps more  think about you often.<br/>
+        Through the reasons are changing, the inner self of you becoming responsible. I know you have to acheieve more in Future and im happy to be part of your success<br/>
+        @your's Chitti
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
